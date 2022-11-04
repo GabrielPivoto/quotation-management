fc7b6ebbd821f4a357cd78fd66219001564de7b4
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-11-04 15:40:48 -0300

    :books: Swagger configuration

---
+++ src/main/java/br/com/inatel/quotationManagement/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package br.com.inatel.quotationManagement.config;
+
+import org.aspectj.bridge.Message;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket SwaggerConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .ignoredParameterTypes(Message.class);
+    }
+
+}
