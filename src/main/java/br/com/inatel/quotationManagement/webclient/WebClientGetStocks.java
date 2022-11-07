d59d93f735d65b60a239eeb442617a698929946a
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-11-07 13:08:04 -0300

    :construction: Adding URL_MANAGER

--- src/main/java/br/com/inatel/quotationManagement/webclient/WebClientGetStocks.java
+++ src/main/java/br/com/inatel/quotationManagement/webclient/WebClientGetStocks.java
@@ -1,6 +1,7 @@
 package br.com.inatel.quotationManagement.webclient;
 
 import br.com.inatel.quotationManagement.model.entity.StockAux;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -16,10 +17,13 @@ import java.util.List;
 @Service
 public class WebClientGetStocks {
 
+    @Value("${URL_MANAGER}")
+    private String URL_MANAGER;
+
     public List<StockAux> listAllStocks(){
         List<StockAux> stocks = new ArrayList<>();
 
-        Flux<StockAux> fluxStock = WebClient.create("http://localhost:8080")
+        Flux<StockAux> fluxStock = WebClient.create(URL_MANAGER)
                 .get()
                 .uri("/stock")
                 .retrieve()
